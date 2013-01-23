302946206deb0841797565fcd2bfd4df394311e8
Michael <maune@isn-oldenburg.de>
2013-01-23 17:21:46 +0100

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ThreeGoodThings::Application.config.secret_token = 'd3f115e4110f4985e4f0a132600a965654ad178d1ad6091f6af6156b5c2d3a7e16d897dffe94978999572757cf2d6131ec24c95ddcbf113c71ca85c9e1545ff5'
